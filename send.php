9fb65045fd5cf68d0d0fbc710175fde19565fdd6
NicolasDelille <nicolas.delille@gmail.com>
2015-08-07 14:06:36 +0200

    update send message process

--- send.php
+++ send.php
@@ -1,3 +1,35 @@
 <?php
+	session_start();
+	include ('db.php');
+	if (!empty($_POST)) {
+		$message = trim(strip_tags($_POST['message']));
+		$username = $_SESSION['username'];
+		$error = "";
+		$microtime_posted = microtime(true);
+
+		if (empty($_POST["message"])) {
+			$error = "Vous n'avez rien posté !";
+			// echo $error;
+			$_SESSION['error'] = $error;
+			header("location: chat.php");
+			die();
+		}
+		else{
+			if (!$error) {
+				$sql = "INSERT INTO messages(id, message, username, date_posted, microtime_posted) 
+						VALUES (NULL,:message,:username,NOW(),$microtime_posted)";
+				$sth = $dbh->prepare($sql);
+				$sth->bindValue(':message',$message);
+				$sth->bindValue(':username',$username);
+				$sth->execute();
+
+			header("location: chat.php");
+			die();
+			}
+			
+
+		}
+		
+	}
 
 	
